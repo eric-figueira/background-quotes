1e7f355a0a776c414614fa82d39719e5aeb84867
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-11-24 22:56:12 -0300

    feat: add disabled button state

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps, ReactNode } from "react";
+import { cn } from "../utils/cn";
 
 interface ButtonProps extends ComponentProps<'button'> {
   children: ReactNode
@@ -6,12 +7,16 @@ interface ButtonProps extends ComponentProps<'button'> {
 
 export function Button({
   children,
+  disabled,
   ...props
 }: ButtonProps) {
   return (
     <button
       {...props}
-      className="rounded-md bg-slate-800 hover:bg-slate-900 transition text-slate-100 font-normal text-sm sm:text-md w-full py-2 md:py-3"
+      className={cn(
+        "rounded-md bg-slate-800 hover:bg-slate-900 transition text-slate-100 font-normal text-sm sm:text-md w-full py-2 md:py-3",
+        disabled && "bg-slate-600 hover:bg-slate-600 cursor-default text-slate-300"
+      )}
     >
       {children}
     </button>
--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -1,7 +1,19 @@
+import { ArrowRight, Loader2 } from "lucide-react";
 import { Button } from "../../components/button";
 import { Slogan } from "./components/slogan";
+import { useState } from "react";
 
 export function LandingPage() {
+  const [isLoading, setIsLoading] = useState(false)
+
+  function handleClick() {
+    setIsLoading(true)
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 5000)
+  }
+
   return (
     <div className="h-screen flex justify-center items-center bg-grid-black/[0.05]">
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]" />
@@ -11,9 +23,24 @@ export function LandingPage() {
 
         <div className="max-w-md md:max-w-lg flex flex-col items-center gap-8 px-6">
           <p className="text-slate-600 font-medium sm:text-lg lg:text-xl text-center max-w-lg">Select the colors, the author, the device, and everything else that you possibly need.</p>
+
           <div className="w-full px-6 sm:px-12">
-            <Button>
-              Start creating
+            <Button 
+              onClick={handleClick}
+              disabled={isLoading}
+            >
+              <div className="flex justify-center items-center gap-2">
+                {isLoading ? (
+                  <>
+                    <Loader2 className="animate-spin size-5" />
+                  </>
+                ) : (
+                  <>
+                    <span>Start creating</span>
+                    <ArrowRight className="size-5" />
+                  </>
+                )}
+              </div>
             </Button>
           </div>
         </div>
