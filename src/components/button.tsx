96b666799cc636a515d1425d2d7f50cb0bf0f56e
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-11-27 17:34:06 -0300

    feat: create link component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,7 +6,7 @@ const Button = React.forwardRef<HTMLButtonElement, React.ComponentProps<'button'
     return (
       <button
         className={cn(
-          "rounded-md bg-slate-800 hover:bg-slate-900 transition text-slate-100 font-normal text-sm sm:text-md w-full py-2 md:py-3 disabled:opacity-50 disabled:pointer-events-none",
+          "rounded-md bg-slate-800 hover:bg-slate-900 transition text-slate-100 font-normal text-sm sm:text-md w-full py-2 md:py-3 disabled:opacity-80 disabled:pointer-events-none",
         )}
         ref={ref}
         {...props}
---
+++ src/components/link.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { cn } from "../utils/cn";
+
+const Link = React.forwardRef<HTMLAnchorElement, React.ComponentProps<'a'>>(
+  ({ href = "/", rel = "noreferrer noopener", target = "_blank", children, className, ...props }, ref) => {
+    return (
+      <a
+        href={href}
+        ref={ref}
+        className={cn(
+          "no-underline text-inherit",
+          className
+        )}
+        target={target}
+        rel={rel}
+        {...props}
+      >
+        {children}
+      </a>
+    )
+  }
+)
+
+export {Link}
