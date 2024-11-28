e7ce393bf408a4a2dcdec5bbdbe825dfd7a85505
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-11-28 17:30:40 -0300

    chore: add radix select and switch

--- src/pages/create-page/index.tsx
+++ src/pages/create-page/index.tsx
@@ -8,19 +8,27 @@ export function CreatePage() {
       <Header />
       <div className="flex-grow p-10">
         <div className="rounded-md border border-slate-300 space-y-4 p-6 w-1/3">
-          <div>
+          <div className="space-y-[6px]">
             <h2 className="font-semibold text-2xl">Quote</h2>
-            <div className="w-full flex items-center gap-4">
-              <div className="w-full flex flex-col gap-[6px]">
-                <span className="font-medium text-sm">Min length</span>
-                <Input type="number" placeholder="e.g. 20" />
+
+            <div className="flex flex-col gap-4">
+
+              
+
+              <div className="w-full flex items-center gap-4">
+                <div className="w-full flex flex-col gap-[6px]">
+                  <span className="font-medium text-sm">Min length</span>
+                  <Input type="number" placeholder="e.g. 20" />
+                </div>
+
+                <div className="w-full flex flex-col gap-[6px]">
+                  <span className="font-medium text-sm">Max length</span>
+                  <Input type="number" placeholder="e.g. 80" />
+                </div>
               </div>
 
-              <div className="w-full flex flex-col gap-[6px]">
-                <span className="font-medium text-sm">Max length</span>
-                <Input type="number" placeholder="e.g. 80" />
-              </div>
             </div>
+
           </div>
 
           {/* <div className="w-full h-[1px] bg-slate-300" />
