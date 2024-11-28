fc9135861e3c286f25fdb3f510f58d6eb376e5a6
Éric Carvalho Figueira <eric.figueira.07@gmail.com>
2024-11-27 21:45:54 -0300

    refactor: update slogan

--- src/pages/landing-page/components/slogan.tsx
+++ src/pages/landing-page/components/slogan.tsx
@@ -1,21 +1,20 @@
 import { useEffect, useState } from "react"
 import { motion } from "framer-motion"
 
-const words = [
-  "guides",
-  "inspire",
-  "challenges",
-  "motivates",
-  "empowers",
-  "moves",
-]
+const words = {
+  "guides": "text-blue-700",
+  "motivates": "text-indigo-700",
+  "inspires": "text-green-700",
+  "challenges": "text-orange-700",
+  "empowers": "text-red-700",
+}
 
 export function Slogan() {
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState<number>(0)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex((prev) => (prev + 1) % words.length);
+      setIndex((prev) => (prev + 1) % Object.keys(words).length);
     }, 3000);
 
     return () => clearInterval(interval);
@@ -29,18 +28,27 @@ export function Slogan() {
           <motion.div
             key={index}
             initial={{ 
-              opacity: "0%",
+              opacity: 0,
+              scale: 0.8,
               y: "50%" 
             }}
             animate={{ 
-              opacity: "100%",
+              opacity: 1,
+              scale: 1,
               y: "0%" 
             }}
-            exit={{ y: "-100%" }}
-            transition={{ duration: 0.6 }}
+            exit={{ 
+              opacity: 0,
+              scale: 0.8
+            }}
+            transition={{ 
+              duration: 0.8,
+              type: "spring",
+              bounce: 0.45
+            }}
             style={{ position: "relative" }}
           >
-            <span className="text-blue-700">{words[index]}</span>
+            <span className={Object.values(words)[index]}>{Object.keys(words)[index]}</span>
           </motion.div>
         </div>
         <span>you.</span>
